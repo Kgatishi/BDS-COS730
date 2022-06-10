fd2359c8e7ceb9c6dee786abd0fc931b57a9d631
Kgatishi <u15209190@tuks.co.za>
2022-06-10 23:00:44 +0200

    creating views and updates to database

--- src/usermanager/urls.py
+++ src/usermanager/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('account', views.index, name='index'),
+    path('home', views.index, name='index'),
     path('signin', views.login, name='login'),
+    path('signout', views.logout, name='logout'),
     path('signup', views.register, name='register'),
+    path('scm', views.scm, name='scm'),
+    path('crm', views.crm, name='crm'),
 ]
--- src/usermanager/views.py
+++ src/usermanager/views.py
@@ -1,19 +1,61 @@
+from multiprocessing import context
+from pickle import TRUE
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
+from .models import User, Account
+from .forms import UserForm, AccountForm
 
+login_ = False
 
-# Create your views here.
 def index(request):
+    if request.method == 'POST':
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('')
+    else:
+        userform = UserForm()
+        accountform = AccountForm()
+    print("-----------------------------")
+    print(login_)
+    print("------------------------------")
+    context = {
+        'userinfo': userform,
+        'useraccount': accountform,
+        'session': login_,
+        'username': 'Tlou'
+    }
+
     template = loader.get_template('landing.html')
-    return HttpResponse(template.render())
+    return HttpResponse(template.render( context, request ))
 
 def login(request):
-    template = loader.get_template('login.html')
-    return HttpResponse(template.render())
+    login_ = True
+    print("-------login(request)--------------")
+    print(login_)
+    print("------------------------------")
+    return HttpResponseRedirect(reverse('index'))
+
+def logout(request):
+    login_ = False
+    return HttpResponseRedirect(reverse('index'))
 
 def register(request):
+    login_ = False
+    return HttpResponseRedirect(reverse('index'))
+
+
+def bookings(request):
     template = loader.get_template('register.html')
     return HttpResponse(template.render())
 
+def scm(request):
+    template = loader.get_template('scm.html')
+    return HttpResponse(template.render())
+
+def crm(request):
+    template = loader.get_template('crm.html')
+    return HttpResponse(template.render())
